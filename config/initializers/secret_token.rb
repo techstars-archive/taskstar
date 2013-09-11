8d13803f9630770e54ed39e719fa3c1aede61f31
theverything <j3ffhorn@gmail.com>
2013-09-11 16:45:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackstar::Application.config.secret_key_base = '61f6bc5763d69c60d9b1bc8213fc3fe18ad919d4efe9e8f5e8f12581747bcd3c673b1501615f3b171d5c874c4113a806d65809b66e7cb343e8fb76b13153b84b'
